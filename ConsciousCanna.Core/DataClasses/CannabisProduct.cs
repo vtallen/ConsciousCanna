7f94d3aff9d751777770d39bde78dda37889682e
Vincent Allen <vallen@gatewayticketing.com>
2025-07-16 12:57:01 -0400

    inital commit

---
+++ ConsciousCanna.Core/DataClasses/CannabisProduct.cs
@@ -0,0 +1,81 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ConsciousCanna.Core.DataClasses;
+
+public static class Species
+{
+    public static IEnumerable<string> Values => 
+    [
+        None,
+        Indica,
+        Sativa,
+        Hybrid 
+    ]; 
+
+    public static readonly string None = "None";
+    public static readonly string Indica = "Indica";
+    public static readonly string Sativa = "Sativa";
+    public static readonly string Hybrid = "Hybrid";
+}
+
+public static class Chemotype
+{
+    public static IEnumerable<string> Values =>
+    [
+        THC,
+        THCCBD,
+        CBD
+    ];
+
+    public static readonly string THC = "1 (THC)";
+    public static readonly string THCCBD = "2 (THC + CBD)";
+    public static readonly string CBD = "3 (CBD)";
+}
+
+public static class ProductType
+{
+    public static IEnumerable<string> Values =>
+    [
+        Flower,
+        Cartridge,
+        Concentrate,
+        Edibles,
+        Tincture,
+        Topical
+    ];
+
+    public static readonly string None = "None";
+    public static readonly string Flower = "Flower";
+    public static readonly string Cartridge = "Cartridge";
+    public static readonly string Concentrate = "Concentrate";
+    public static readonly string Edibles = "Edible";
+    public static readonly string Tincture = "Tincture";
+    public static readonly string Topical = "Topical";
+}
+
+public class CannabisProduct
+{
+    public required string ProductName { get; set; }
+
+    public string? ProductBrand { get; set; }
+
+    public string? Grower { get; set; }
+
+    public string? Strain { get; set; }
+
+    public string? ProductSpecies { get; set; }
+
+    public string? ProductChemotype { get; set; }
+
+    public string? PurchaseLocation { get; set; }
+
+    public DateTime? PurchaseDate { get; set; }
+
+    public DateTime? HarvestDate { get; set; }
+
+    public double Cost { get; set; }
+
+    public string? Notes { get; set; }
+
+    public TestingData? TestingData { get; set; }
+}
---
+++ ConsciousCanna.Core/DataClasses/TestingData.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsciousCanna.Core.DataClasses;
+
+public enum TestingUnit
+{
+    None,
+    Percent,
+    Mg
+}
+
+public class TestingData
+{
+    public required TestingUnit Unit { get; set; }
+
+    public double THC { get; set; }
+
+    public double THCA { get; set; }
+
+    public double THCP { get; set; }
+
+    public double THCV { get; set; }
+
+    public double THCVA { get; set; }
+
+    public double D8THC { get; set; }
+
+    public double CBD { get; set; }
+
+    public double CBDA { get; set; }
+
+    public double CBN { get; set; }
+
+    public double CBG { get; set; }
+
+    public double CBGA { get; set; }
+
+    public double CBC { get; set; }
+
+    // Terpenes 
+
+    public double Camphene { get; set; }
+
+    public double bCaryophyllene { get; set; }
+
+    public double aBisabolol { get; set; }
+
+    public double CaryophylleneOxide { get; set; }
+
+    public double Farnesene { get; set; }
+
+    public double Fenchol { get; set; }
+
+    public double Garaniol { get; set; }
+
+    public double Guaiol { get; set; }
+
+    public double aHumulene { get; set; }
+
+    public double Linalool { get; set; }
+
+    public double Limonene { get; set; }
+
+    public double Mrycene { get; set; }
+
+    public double Nerolidol { get; set; }
+
+    public double Ocimene { get; set; }
+
+    public double aPhellandrene { get; set; }
+
+    public double aPinene { get; set; }
+
+    public double bPinene { get; set; }
+
+    public double aTerpinene { get; set; }
+
+    public double Terpinolene { get; set; }
+
+    public double Terpineol { get; set; }
+
+    public double Valencene { get; set; }
+}
