6de0f2324f07992912ae13118d51bf6aa8731aaa
Vincent Allen <vallen@gatewayticketing.com>
2025-07-16 13:13:52 -0400

    Added IndexedDB dependancy for local storage

--- ConsciousCanna.Blazor/ConsciousCanna.Blazor/obj/Debug/net9.0/ConsciousCanna.Blazor.AssemblyInfo.cs
+++ ConsciousCanna.Blazor/ConsciousCanna.Blazor/obj/Debug/net9.0/ConsciousCanna.Blazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsciousCanna.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f94d3aff9d751777770d39bde78dda37889682e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsciousCanna.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsciousCanna.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
